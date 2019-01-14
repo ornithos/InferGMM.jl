b10e38324ae98e73458004f06a8e26e31de460ca
Alex Bird <abird@turing.ac.uk>
2019-01-14 19:35:18 +0000

    fix deps (sobol/Normal)

--- src/scm.jl
+++ src/scm.jl
@@ -3,7 +3,7 @@ module scm
 using ..gmm
 using LinearAlgebra
 using Parameters, Formatting
-using Distributions: ncomponents
+using Distributions: ncomponents, Normal
 using Flux
 using Flux: Tracker
 using Sobol
